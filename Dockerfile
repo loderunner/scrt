ed9794d4116aa2243d735a26653ffce44bb006a0
Charles Francoise <charles.francoise@gmail.com>
2022-05-19 12:12:34 +0200

    first docs import

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM scratch
-ENTRYPOINT ["/scrt"]
-COPY scrt /
---
+++ assets/index.html.a1f159a2.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./app.6f7fd6b9.js";const _={};function r(n,t){return null}var f=e(_,[["render",r],["__file","index.html.vue"]]);export{f as default};
