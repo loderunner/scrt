8446c22ace5c6d971f840d2e366d35e146f8dde5
Charles Francoise <charles.francoise@gmail.com>
2022-05-23 20:12:43 +0200

    add installation docs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,153 +3,176 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { viteBundler } from 'vuepress-vite'
 import path from 'path'
 
+import type { HeadConfig, NavbarConfig, SidebarConfig } from 'vuepress-vite'
+
 const title = 'scrt'
 const description = 'The secret manager for the command line'
 const baseURL = 'https://scrt.run'
+
+const head: HeadConfig[] = [
+  [
+    'link',
+    {
+      rel: 'apple-touch-icon',
+      sizes: '57x57',
+      href: '/images/favicon-57.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'apple-touch-icon',
+      sizes: '60x60',
+      href: '/images/favicon-60.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'apple-touch-icon',
+      sizes: '72x72',
+      href: '/images/favicon-72.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'apple-touch-icon',
+      sizes: '76x76',
+      href: '/images/favicon-76.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'apple-touch-icon',
+      sizes: '114x114',
+      href: '/images/favicon-114.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'apple-touch-icon',
+      sizes: '120x120',
+      href: '/images/favicon-120.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'apple-touch-icon',
+      sizes: '144x144',
+      href: '/images/favicon-144.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'apple-touch-icon',
+      sizes: '152x152',
+      href: '/images/favicon-152.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/images/favicon-180.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '192x192',
+      href: '/images/favicon-192.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/images/favicon-32.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '96x96',
+      href: '/images/favicon-96.png',
+    },
+  ],
+  [
+    'link',
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/images/favicon-16.png',
+    },
+  ],
+  ['meta', { name: 'msapplication-TileColor', content: '#ffffff' }],
+  [
+    'meta',
+    { name: 'msapplication-TileImage', content: '/images/favicon-144.png' },
+  ],
+  ['meta', { name: 'theme-color', content: '#ffffff' }],
+  ['meta', { property: 'og:type', content: 'website' }],
+  ['meta', { property: 'og:url', content: baseURL }],
+  ['meta', { property: 'og:title', content: title }],
+  ['meta', { property: 'og:description', content: description }],
+  ['meta', { property: 'og:image', content: `${baseURL}/images/social.png` }],
+  ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
+  ['meta', { property: 'twitter:url', content: baseURL }],
+  ['meta', { property: 'twitter:title', content: title }],
+  ['meta', { property: 'twitter:description', content: description }],
+  [
+    'meta',
+    {
+      property: 'twitter:image',
+      content: `${baseURL}/images/social.png`,
+    },
+  ],
+]
+
+const navbar: NavbarConfig = [
+  {
+    text: 'Guide',
+    link: '/guide/',
+  },
+]
+
+const sidebar: SidebarConfig = {
+  '/guide': [
+    {
+      text: 'Guide',
+      children: ['/guide/README.md', '/guide/installation.md'],
+    },
+  ],
+}
 
 export default defineUserConfig({
   lang: 'en-US',
   title,
   description,
-  head: [
-    [
-      'link',
-      {
-        rel: 'apple-touch-icon',
-        sizes: '57x57',
-        href: '/images/favicon-57.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'apple-touch-icon',
-        sizes: '60x60',
-        href: '/images/favicon-60.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'apple-touch-icon',
-        sizes: '72x72',
-        href: '/images/favicon-72.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'apple-touch-icon',
-        sizes: '76x76',
-        href: '/images/favicon-76.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'apple-touch-icon',
-        sizes: '114x114',
-        href: '/images/favicon-114.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'apple-touch-icon',
-        sizes: '120x120',
-        href: '/images/favicon-120.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'apple-touch-icon',
-        sizes: '144x144',
-        href: '/images/favicon-144.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'apple-touch-icon',
-        sizes: '152x152',
-        href: '/images/favicon-152.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/images/favicon-180.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '192x192',
-        href: '/images/favicon-192.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/images/favicon-32.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '96x96',
-        href: '/images/favicon-96.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/images/favicon-16.png',
-      },
-    ],
-    ['meta', { name: 'msapplication-TileColor', content: '#ffffff' }],
-    [
-      'meta',
-      { name: 'msapplication-TileImage', content: '/images/favicon-144.png' },
-    ],
-    ['meta', { name: 'theme-color', content: '#ffffff' }],
-    ['meta', { property: 'og:type', content: 'website' }],
-    ['meta', { property: 'og:url', content: baseURL }],
-    ['meta', { property: 'og:title', content: title }],
-    ['meta', { property: 'og:description', content: description }],
-    ['meta', { property: 'og:image', content: `${baseURL}/images/social.png` }],
-    ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
-    ['meta', { property: 'twitter:url', content: baseURL }],
-    ['meta', { property: 'twitter:title', content: title }],
-    ['meta', { property: 'twitter:description', content: description }],
-    [
-      'meta',
-      {
-        property: 'twitter:image',
-        content: `${baseURL}/images/social.png`,
-      },
-    ],
-  ],
+  head,
 
   base: '/',
 
   bundler: viteBundler({}),
   theme: defaultTheme({
     logo: '/images/logo.svg',
+    navbar,
+    sidebar,
   }),
+
   alias: {
     '@theme/HomeFeatures.vue': path.resolve(
       __dirname,
---
+++ docs/guide/installation.md
@@ -0,0 +1,88 @@
+# Installation
+
+## Download binary release
+
+Download the latest binary release for your platform from the [releases page](https://github.com/loderunner/scrt/releases). Decompress the archive to the desired location. E.g.
+
+```shell
+tar xzvf scrt_0.3.2_linux_x86_64.tar.gz
+sudo cp scrt_0.3.2_linux_x86_64/scrt /usr/local/bin/scrt
+```
+
+## apt (Debian/Ubuntu)
+
+Configure the apt repository:
+
+```shell
+echo "deb [signed-by=/usr/share/keyrings/scrt-archive-keyring.gpg] https://apt.scrt.run /" \
+  | sudo tee /etc/apt/sources.list.d/scrt.list
+curl "https://apt.scrt.run/key.gpg" \
+  | gpg --dearmor \
+  | sudo tee /usr/share/keyrings/scrt-archive-keyring.gpg > /dev/null
+```
+
+Install the binary package:
+
+```shell
+sudo apt update
+sudo apt install scrt
+```
+
+## yum (RHEL/CentOS)
+
+Configure the yum repository, in `/etc/yum.repos.d/scrt.repo`:
+
+```ini
+[scrt]
+name=scrt
+baseurl=https://yum.scrt.run
+repo_gpgcheck=1
+gpgcheck=1
+enabled=1
+gpgkey=https://yum.scrt.run/key.gpg
+sslverify=1
+metadata_expire=300
+```
+
+Install the binary package
+
+```shell
+sudo yum update
+sudo yum install scrt
+```
+
+## Homebrew (macOS)
+
+Configure the Homebrew tap:
+
+```shell
+brew tap loderunner/scrt
+```
+
+Install the binary package:
+
+```
+brew install scrt
+```
+
+## go get
+
+Use `go get` to download and build the latest version:
+
+```shell
+go get github.com/loderunner/scrt
+```
+
+`scrt` will be available in the binaries directory of your GOPATH. Add it to your path, and run `scrt`.
+
+## Build from source
+
+Clone the repository and use `go build` to build a binary (requires go >= 1.16):
+
+```shell
+git clone https://github.com/loderunner/scrt.git
+cd scrt
+go build .
+```
+
+The built executable will be located at `scrt` at the root of the repository.
