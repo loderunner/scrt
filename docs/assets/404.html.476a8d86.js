486d1575798c804b2c80490476ba4109551f9c8f
Charles Francoise <charles.francoise@gmail.com>
2022-06-06 17:40:53 +0200

    update documentation

---
+++ docs/assets/404.html.476a8d86.js
@@ -0,0 +1,1 @@
+import{_}from"./app.ce2c9135.js";const e={};function r(t,c){return null}var f=_(e,[["render",r],["__file","404.html.vue"]]);export{f as default};
---
+++ docs/assets/404.html.bb2cf2d7.js
@@ -0,0 +1,1 @@
+const t={key:"v-3706649a",path:"/404.html",title:"",lang:"en-US",frontmatter:{layout:"404"},excerpt:"",headers:[],git:{},filePathRelative:null,analytics:{dataDomain:"scrt.run",src:"https://analytics.scrt.run/js/plausible.js"}};export{t as data};
