65482b8b6c265239a5f534a2ef3aae3929ad8bce
Charles Francoise <charles.francoise@gmail.com>
2022-06-06 17:53:14 +0200

    docs: add analytics to documentation website (#92)
    * create analytics plugin
    
    * don't run analytics plugin in dev mode
    
    * don't add analytics during SSR
    
    * allow es module interop

---
+++ docs/.vuepress/plugins/analytics/client/enhance.ts
@@ -0,0 +1,20 @@
+import { defineClientAppSetup, usePageData } from '@vuepress/client'
+
+import type { AnalyticsPageData } from '../node'
+
+export default defineClientAppSetup(() => {
+  if (__VUEPRESS_SSR__) return
+
+  const pageData = usePageData<AnalyticsPageData>()
+  const { analytics } = pageData.value
+
+  if (!analytics) {
+    return
+  }
+
+  const script = document.createElement('script')
+  script.defer = true
+  script.src = analytics.src
+  script.setAttribute('data-domain', analytics.dataDomain)
+  document.head.appendChild(script)
+})
