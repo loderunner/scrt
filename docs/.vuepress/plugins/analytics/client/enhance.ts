35b29d86b3d915d6d1c69f9738119c0794a61585
Charles Francoise <charles.francoise@gmail.com>
2022-06-20 19:01:04 +0200

    docs: fix analytics plugin (#132)

--- docs/.vuepress/plugins/analytics/client/enhance.ts
+++ docs/.vuepress/plugins/analytics/client/enhance.ts
@@ -1,20 +1,22 @@
-import { defineClientAppSetup, usePageData } from '@vuepress/client'
+import { defineClientConfig, usePageData } from '@vuepress/client'
 
 import type { AnalyticsPageData } from '../node'
 
-export default defineClientAppSetup(() => {
-  if (__VUEPRESS_SSR__) return
+export default defineClientConfig({
+  setup: () => {
+    if (__VUEPRESS_SSR__) return
 
-  const pageData = usePageData<AnalyticsPageData>()
-  const { analytics } = pageData.value
+    const pageData = usePageData<AnalyticsPageData>()
+    const { analytics } = pageData.value
 
-  if (!analytics) {
-    return
-  }
+    if (!analytics) {
+      return
+    }
 
-  const script = document.createElement('script')
-  script.defer = true
-  script.src = analytics.src
-  script.setAttribute('data-domain', analytics.dataDomain)
-  document.head.appendChild(script)
+    const script = document.createElement('script')
+    script.defer = true
+    script.src = analytics.src
+    script.setAttribute('data-domain', analytics.dataDomain)
+    document.head.appendChild(script)
+  },
 })
